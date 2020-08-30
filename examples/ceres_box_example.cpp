17faa51df2803f44ab2036de2fb0939998c40f12
gautams3 <gautams3@users.noreply.github.com>
2020-08-30 16:31:28 -0700

    box example: use vectors instead of arrays
    also using finer discretization in time

--- examples/ceres_box_example.cpp
+++ examples/ceres_box_example.cpp
@@ -8,12 +8,16 @@
 #include "glog/logging.h"
 #include "math.h"
 
-constexpr std::size_t kTimesteps = 10;
+constexpr std::size_t kTimesteps = 20;
 constexpr double dt = 1.0/ kTimesteps;
 constexpr std::size_t state_dim = 2; // x, xdot
 constexpr double m = 1.0; // unit mass
 constexpr double mu = 0.0;
 constexpr double g = 9.81; // m/s^2
+
+constexpr double init_x = 0.0;
+constexpr double init_xdot = 0.0;
+constexpr double init_u = 0.0;
 
 template<typename T> T sign(const T& x) {
   if (x > T(0)) {
@@ -30,12 +34,13 @@ template<typename T> T sign(const T& x) {
 namespace plt = matplotlibcpp;
 
 template <typename T>
-void plot_trajectory(const T states) {
-  int state_dim = 2;
+void plot_trajectory(const std::vector<std::vector<T>> states) {
+  int traj_length = states.size();
+  int state_dim = states[0].size();
   for (int i = 0; i < state_dim; ++i) {
-    std::vector<double> traj(kTimesteps);
-    for (int t = 0; t < kTimesteps; ++t) {
-      traj[t] = static_cast<double>(states[t*state_dim + i]);
+    std::vector<double> traj(traj_length);
+    for (int t = 0; t < traj_length; ++t) {
+      traj[t] = static_cast<double>(states[t][i]);
     }
     plt::named_plot("state[" + std::to_string(i) + "]", traj);
   }
@@ -45,43 +50,44 @@ void plot_trajectory(const T states) {
 #endif
 
 template <typename T>
-T rollout(const T* inputs, T* states, const bool doprint = false) {
+T rollout(const std::vector<T> &inputs, std::vector<std::vector<T>> &states, const bool doprint = false) {
+  const std::size_t traj_length = states.size();
   const T goal_position = T(1.0);
   const T goal_velocity = T(0.0);
-  states[0] = T(0.0); // start from 0 position
-  states[1] = T(0.0); // start with 0 velocity
+  std::vector<T> init_state = {T(init_x), T(init_xdot)};
+  states[0] = init_state;
   T friction = T(0.0);
   T reg_input = T(0.0); //input regularization
 
   for (size_t i = 1; i < kTimesteps; i++) {
     reg_input = reg_input + pow(inputs[i-1], 2.0);
     size_t idx = i * state_dim, prev_idx = (i-1) * state_dim;
-    const T v_prev = states[prev_idx + 1], x_prev = states[prev_idx + 0];
+    const T v_prev = states[i-1][1], x_prev = states[i-1][0];
     if (v_prev == T(0)) { //static friction: opposite to force
       friction = -sign(inputs[i-1]) * std::min(abs(inputs[i-1]), T(mu * m * g));
     } else {  //kinetic friction: opposite to motion
       friction = -sign(v_prev) * T(mu * m * g);
     }
-    states[idx + 0] = x_prev + v_prev * T(dt);
-    states[idx + 1] = v_prev + (inputs[i-1] + friction)/m * T(dt);
+    states[i][0] = x_prev + v_prev * T(dt);
+    states[i][1] = v_prev + (inputs[i-1] + friction)/m * T(dt);
 
     //Account for friction having a larger effect due to time discretization
     if (abs(friction) > abs(inputs[i-1])) {
-      if(doprint && (states[idx + 1] < T(0))) {
-        printf("Friction moving body in opposite direction! Clip velocity %.3f below to 0\n", states[idx + 1]);
+      if(doprint && (states[i][1] < T(0))) {
+        printf("Friction moving body in opposite direction! Clip velocity %.3f below to 0\n", states[i][1]);
       }
-      states[idx + 1] = std::max(states[idx + 1], T(0));
+      states[i][1] = std::max(states[i][1], T(0));
     }
 
     //Logging
     if (doprint) {
-      printf("%lu: x %.3f, xdot %.3f, accn %.3f, u %.3f\t", i, states[idx+0], states[idx+1], (inputs[i-1] + friction)/m, inputs[i]);
+      printf("%lu: x %.3f, xdot %.3f, accn %.3f, u %.3f\t", i, states[i][0], states[i][1], (inputs[i-1] + friction)/m, inputs[i]);
       printf("friction = %.3f\n", friction);
     }
   }
 
-  T error_pos = pow(states[(kTimesteps-1) * state_dim + 0] - goal_position, 2.0);
-  T error_vel = pow(states[(kTimesteps-1) * state_dim + 1] - goal_velocity, 2.0);
+  T error_pos = pow(states[kTimesteps-1][0] - goal_position, 2.0);
+  T error_vel = pow(states[kTimesteps-1][1] - goal_velocity, 2.0);
   T error = T(0.0);
   error += error_pos; 
   error += T(0.02) * error_vel;
@@ -90,19 +96,20 @@ T rollout(const T* inputs, T* states, const bool doprint = false) {
   return error;
 }
 
-void print_trajectory(const double *states, const double *inputs) {
+void print_trajectory(const std::vector<std::vector<double>> &states, const std::vector<double> &inputs) {
   for (size_t i = 0; i < kTimesteps; i++) {
-    size_t idx = i * state_dim;
-    printf("%lu: x %.3f, xdot %.3f, u %.3f\n", i, states[idx+0], states[idx+1], inputs[i]);
+    printf("%lu: x %.3f, xdot %.3f, u %.3f\n", i, states[i][0], states[i][1], inputs[i]);
   }
 }
 
 struct CeresFunctional
 {
   template <typename T>
-  bool operator()(const  T* inputs, T* residual) const {
-    T dummy_states[kTimesteps * state_dim];
-    *residual = rollout<T>(inputs, dummy_states);
+  bool operator()(const T* inputs, T* residual) const {
+    std::vector<T>input_vec(inputs, inputs+kTimesteps); //vector from input array
+    std::vector<T> init_traj(state_dim, T(0)); //initialize dummy_states with 0s
+    std::vector<std::vector<T>> dummy_states(kTimesteps, init_traj);
+    *residual = rollout<T>(input_vec, dummy_states);
     return true;
   }
 };
@@ -111,31 +118,20 @@ int main(int argc, char** argv) {
   google::InitGoogleLogging(argv[0]); // TODO: what's the API for glog? using printf for now
 
   ceres::Problem problem;
-  const int state_dim = 2;
-  double states[kTimesteps*state_dim];
-  double inputs[kTimesteps];
 
-  // INITIALIZE
-  double init_x = 0.0;
-  double init_xdot = 0.0;
-  double init_u = 0.0;
-
-  for (size_t i = 0; i < kTimesteps; i++) {
-    size_t idx = i * state_dim;
-    states[idx + 0] = init_x;
-    states[idx + 1] = init_xdot;
-    inputs[i] = init_u;
-  }
+  std::vector<double> init_traj = {init_x, init_xdot};
+  std::vector<std::vector<double>> states(kTimesteps, init_traj); //initialize traj
+  std::vector<double> inputs(kTimesteps, init_u);
   printf("Initialize Trajectory\n");
   print_trajectory(states, inputs);
 
   ceres::CostFunction* cost_function = new ceres::AutoDiffCostFunction<CeresFunctional, 1, kTimesteps>(new CeresFunctional);
 
-  problem.AddResidualBlock(cost_function, NULL, inputs);
+  problem.AddResidualBlock(cost_function, NULL, inputs.data());
 
   ceres::Solver::Options options;
   options.minimizer_progress_to_stdout = true;
-  options.max_num_iterations = 300;
+  options.max_num_iterations = 500;
   ceres::Solver::Summary summary;
   ceres::Solve(options, &problem, &summary);
 
